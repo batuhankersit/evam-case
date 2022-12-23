9565da5061f370f1dbeafcdba60e0db9bf832e1f
batuhankersit <batuhan.kersit@volantx.com>
2022-12-23 22:01:06 +0300

    search input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,21 @@
+import {SearchIcon} from "../Icons/SearchIcon";
+import {FC} from "react";
+
+interface Props {
+  setSearchText:(data:string) => void;
+  searchText:string | undefined;
+}
+
+export const SearchInput:FC<Props> = ({searchText,setSearchText}) => {
+
+  return (
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text"><SearchIcon/></span>
+      </div>
+      <input type="text" value={searchText} onChange={e => setSearchText(e.target.value)} className="form-control"
+             placeholder="Search with Form Name"
+             />
+    </div>
+  )
+}
